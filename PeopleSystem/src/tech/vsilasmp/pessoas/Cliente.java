ba8fedd1fc0bc42dad47b1c7985a8aede8a1dd0e
ViniciusSilas01 <vinicius2012silas@gmail.com>
2024-10-23 14:05:44 -0300

    implementado a função atualizar informações

--- PeopleSystem/src/tech/vsilasmp/pessoas/Cliente.java
+++ PeopleSystem/src/tech/vsilasmp/pessoas/Cliente.java
@@ -2,6 +2,7 @@ package tech.vsilasmp.pessoas;
 
 import tech.vsilasmp.composicao.Profissao;
 
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Cliente extends Pessoa {
@@ -38,12 +39,64 @@ public class Cliente extends Pessoa {
         }
     }
 
+    public void atualizarInformações() {
+        System.out.println("Deseja atualizar as informações pessoais? (S/N)");
+        String resposta = leitor.nextLine();
+        if(resposta.equalsIgnoreCase("S")){
+            System.out.println("Informe o nome atualizado: ");
+            this.setNome(leitor.nextLine());
+
+            System.out.println("Deseja atualizar a data de nascimento? (S/N)");
+            resposta = leitor.nextLine();
+            if(resposta.equalsIgnoreCase("S")){
+                System.out.println("Informe o dia do nascimento: ");
+                int dia = leitor.nextInt();
+                System.out.println("Informe o mês de nascimento: ");
+                int mes = leitor.nextInt();
+                System.out.println("Ingorme o ano de nascimento: ");
+                int ano = leitor.nextInt();
+                leitor.nextLine();
+                this.setDataNascimento(LocalDate.of(ano, mes, dia));
+            }
+            System.out.println("Deseja atualizar o endereço? (S/N)");
+            resposta = leitor.nextLine();
+            if (resposta.equalsIgnoreCase("S")){
+                this.getEndereco().cadastrar();
+            }
+            System.out.println("Deseja atualizar o telefone de contato (S/N)");
+            resposta = leitor.nextLine();
+            if (resposta.equalsIgnoreCase("S")){
+                this.getTelsContato().cadastrar();
+            }
+        }
+        System.out.println("Deseja atualizar o código do cliente? (S/N)");
+        resposta = leitor.nextLine();
+        if (resposta.equalsIgnoreCase("S")){
+            System.out.println("Informe o novo código: ");
+            this.codigo = leitor.nextLine();
+        }
+        System.out.println("Deseja atualizar a profissão do cliente? (S/N)");
+        resposta = leitor.nextLine();
+        if (resposta.equalsIgnoreCase("S")){
+            System.out.println("Informe a nova profissão: ");
+            Profissao novaProfissao = Profissao.valueOf(leitor.nextLine());
+            this.setProfissao(novaProfissao);
+
+            if (novaProfissao == Profissao.OUTROS) {
+                System.out.println("Informe a nova profissão personalizada: ");
+                this.tipoProfissaoOutros = leitor.nextLine();
+            } else {
+                this.tipoProfissaoOutros = null;
+            }
+        }
+    }
+
     public String toString() {
         return "Cliente{" +
-            "codigo='" + codigo + '\'' +
-                    ", profissao=" + profissao +
-                    ", tipoProfissaoOutros='" + tipoProfissaoOutros + '\'' +
-                    "} ";
+                "codigo='" + codigo + '\'' +
+                ", profissao=" + profissao +
+                ", tipoProfissaoOutros='" + tipoProfissaoOutros + '\'' +
+                "} ";
     }
 
 }
